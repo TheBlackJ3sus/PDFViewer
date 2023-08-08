ab562c17dab7ae2e8b8efdf35ac31dfbbf68ccc4
Kas <khasimbradley@gmail.com>
2023-08-08 11:20:18 -0400

    add options

--- Sources/PDFViewer/PDFViewer.swift
+++ Sources/PDFViewer/PDFViewer.swift
@@ -1,23 +1,39 @@
 import SwiftUI
 import PDFKit
 
+public enum PDFBuildType {
+    case document(PDFDocument)
+    case images([UIImage])
+}
+
+public struct viewerOptions {
+    let autoScales: Bool
+    let scaleFactor: CGFloat
+    let minScaleFactor: CGFloat
+    let maxScaleFactor: CGFloat
+}
+
 @available(iOS 14, macOS 11.0, *)
 public class PDFController: UIViewController {
-    public var document: PDFDocument
+    let document: PDFDocument
+    let options: viewerOptions
     
-    public init(document: PDFDocument) {
+    
+    
+    init(_ document: PDFDocument, options: viewerOptions) {
         self.document = document
+        self.options = options
         super.init(nibName: nil, bundle: nil)
     }
     //Create document from UIImages
-    public convenience init(images: [UIImage]) {
+    convenience init(_ images: [UIImage], options: viewerOptions) {
         let newDocument = PDFDocument()
         for image in images {
             if let newPage = PDFPage(image: image) {
                 newDocument.insert(newPage, at: newDocument.pageCount)
             }
         }
-        self.init(document: newDocument)
+        self.init(newDocument, options: options)
     }
     
     required init?(coder: NSCoder) {
@@ -31,6 +47,10 @@ extension PDFController {
         let pdfView = PDFView()
         pdfView.translatesAutoresizingMaskIntoConstraints = false
         pdfView.document = self.document
+        pdfView.autoScales = self.options.autoScales
+        pdfView.scaleFactor = self.options.scaleFactor
+        pdfView.minScaleFactor = self.options.minScaleFactor
+        pdfView.maxScaleFactor = self.options.maxScaleFactor
         return pdfView
     }
     private func createPDFThumbnailView(pdfView: PDFView) -> PDFThumbnailView {
@@ -58,29 +78,33 @@ extension PDFController {
     }
 }
 
+
+/// A UIViewController wrapper for PDFController
 @available(iOS 14, macOS 11.0, *)
 public struct PDFViewer: UIViewControllerRepresentable {
-    public var build: PDFBuildType
+    let build: PDFBuildType
+    var options: viewerOptions = viewerOptions(autoScales: false, scaleFactor: 1, minScaleFactor: 1, maxScaleFactor: 4)
     
+    ///Use default options
     public init(build: PDFBuildType) {
         self.build = build
+    }
+    
+    ///Provide custom options
+    public init(_ build: PDFBuildType, options: viewerOptions) {
+        self.build = build
+        self.options = options
     }
     
     public func makeUIViewController(context: Context) -> PDFController {
         switch build {
         case .document(let document):
-            return PDFController(document: document)
+            return PDFController(document, options: options)
         case .images(let images):
-            return PDFController(images: images)
+            return PDFController(images, options: options)
         }
     }
         
     public func updateUIViewController(_ uiViewController: PDFController, context: Context) {
-        // Updates the state of the specified view controller with new information from SwiftUI.
     }
-}
-
-public enum PDFBuildType {
-    case document(PDFDocument)
-    case images([UIImage])
 }
